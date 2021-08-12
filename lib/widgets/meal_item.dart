fa017e1a59f61ad3f89d47b61da3efed681c18b7
Elvis <66243414+Elvis-1@users.noreply.github.com>
2021-08-13 09:04:32 +1300

    added meal_item

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MealItem extends StatelessWidget {
+  //const MealItem({Key? key}) : super(key: key);
+ void selectMeal(){}
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+      ),
+
+    );
+  }
+}
